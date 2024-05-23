0cd33904f65a78e9e9f3ca1803b1e3fc531b2714
Igor <igorivandikov87@gmail.com>
2024-05-23 20:35:58 +0300

    styles task 1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,25 @@
 
+
+
+const classForLi = document.querySelectorAll('.item li');
+
+classForLi.forEach((li) => {
+    li.classList.add('list')
+});
+
+
+
+const classForTitle = document.querySelectorAll('.item h2');
+
+classForTitle.forEach((h2) => {
+    h2.classList.add('list-title')
+});
+
+
+const classForUl = document.getElementById('categories');
+classForUl.classList.add('list-categories');
+
+
+
+
+
