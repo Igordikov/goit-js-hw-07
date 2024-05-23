3bef048b4dda2d1f01fb21d59487c07563fd1c87
Igor <igorivandikov87@gmail.com>
2024-05-23 21:37:15 +0300

    task 1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 
-
+// ======================STYLES==========================
 
 const classForLi = document.querySelectorAll('.item li');
 
@@ -19,7 +19,33 @@ classForTitle.forEach((h2) => {
 const classForUl = document.getElementById('categories');
 classForUl.classList.add('list-categories');
 
+// ///////////////////////STYLES///////////////////////
 
 
+// Порахує й виведе в консоль кількість категорій в 
+// ul#categories, тобто елементів li.item.
+
+
+
+// Для кожного елемента li.item у списку ul#categories 
+// знайде й виведе в консоль текст заголовка елемента 
+// (тегу <h2>) і кількість елементів у категорії (усіх <li>, 
+//     вкладених у нього).
+
+
+
+const listOfCategories = document.querySelectorAll('#categories .item');
+console.log('Number of categories: ', listOfCategories.length)
+
+
+
+const nameOfList = document.querySelectorAll('.item h2');
+nameOfList.forEach((li) => {
+    console.log('Category: ', li.textContent)
+
+
+    const quantityOfLists = li.nextElementSibling.querySelectorAll('li');
+    console.log('Elements: ', quantityOfLists.length);
+         })
 
 
